c306f0dc664e30a21ac17a159a9189bc0f86fb37
lowercasemichael <lowercasemichael@lowercase.com>
2023-08-08 09:00:42 +0200

    added sense hat snippets

---
+++ raspberry-sense-hat/playground-code-snippets/joystick.py
@@ -0,0 +1,15 @@
+# https://projects.raspberrypi.org/en/projects/getting-started-with-the-sense-hat/9
+from sense_hat import SenseHat
+sense = SenseHat()
+
+
+while True:
+
+
+
+
+
+
+
+    for event in sense.stick.get_events():
+        print(event.direction, event.action)
---
+++ raspberry-sense-hat/playground-code-snippets/led-array-test.py
@@ -0,0 +1,7 @@
+from sense_hat import SenseHat 
+
+sense = SenseHat()
+
+sense.clear(0, 0, 0)
+
+sense.set_pixel(0, 0, (96, 192, 64))
