d4fada5cb00c734dd0cace8ee371ec526b82b483
PierreNS <pierreskinnerup@gmail.com>
2023-08-09 12:36:02 +0200

    PushItGood

--- python/raspberry-sense-hat/playground-code-snippets/led-matrix-gamma-profile.py
+++ python/raspberry-sense-hat/playground-code-snippets/led-matrix-gamma-profile.py
@@ -1,4 +1,22 @@
+import time
 from sense_hat import SenseHat
+
 sense = SenseHat()
-pixels = [3*[c] for c in range(64)]
-sense.set_pixels(pixels)
+sense.clear(8, 0, 0)
+
+# print(sense.gamma)
+# time.sleep(2)
+
+#sense.gamma = [31] * 32 #reversed(sense.gamma)
+sense.gamma = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+time.sleep(2)
+print(sense.gamma)
+time.sleep(2)
+
+# sense.low_light = True
+# print(sense.gamma)
+# time.sleep(2)
+
+# sense.low_light = False
+# print(sense.gamma)
+# time.sleep(2)
