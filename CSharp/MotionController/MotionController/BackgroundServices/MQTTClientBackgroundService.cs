a40805ea28208f0dbc974b7dccbed847d6f7ff36
KingoBoiii <kingodk@outlook.dk>
2023-08-10 14:09:43 +0200

    Refactored MQTTClientBackgroundService

--- CSharp/MotionController/MotionController/BackgroundServices/MQTTClientBackgroundService.cs
+++ CSharp/MotionController/MotionController/BackgroundServices/MQTTClientBackgroundService.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MotionController.Data;
 using MotionController.Extensions.Hosting;
 using MotionController.Services;
 using MQTTnet;
 using MQTTnet.Client;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Concurrent;
 using System.Text;
 
@@ -45,26 +48,41 @@ internal class MQTTClientBackgroundService : BackgroundService<MQTTClientBackgro
 {
     private readonly ConcurrentDictionary<string, Type> s_Map = new();
 
-    public MQTTClientBackgroundService(ILogger<MQTTClientBackgroundService> logger, IDeviceSessionService deviceService)
+    public MQTTClientBackgroundService(ILogger<MQTTClientBackgroundService> logger, IServiceProvider serviceProvider)
         : base(logger)
     {
-        DeviceSessionService = deviceService;
+        ServiceProvider = serviceProvider;
     }
 
-    private IDeviceSessionService DeviceSessionService { get; }
+    private IServiceProvider ServiceProvider { get; }
 
     protected override async Task ExecuteLogicAsync(CancellationToken cancellationToken)
     {
         s_Map.TryAdd("encyclopedia/environment", typeof(DeviceEnv));
 
+        var client = await CreateAndConnectMQTTClientAsync();
+
+        client.ApplicationMessageReceivedAsync += OnApplicationMessageReceivedFuncAsync;
+
+        await client.SubscribeAsync("encyclopedia/#", MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce, CancellationToken.None);
+
+        while (cancellationToken.IsCancellationRequested)
+        {
+        }
+    }
+
+    private static async Task<IMqttClient> CreateAndConnectMQTTClientAsync()
+    {
         var factory = new MqttFactory();
 
         var client = factory.CreateMqttClient();
 
-        MqttClientOptionsBuilderTlsParameters tlsOptions = new MqttClientOptionsBuilderTlsParameters();
-        tlsOptions.SslProtocol = System.Security.Authentication.SslProtocols.Tls12;
-        tlsOptions.UseTls = true;
-        tlsOptions.AllowUntrustedCertificates = true;
+        MqttClientOptionsBuilderTlsParameters tlsOptions = new()
+        {
+            SslProtocol = System.Security.Authentication.SslProtocols.Tls12,
+            UseTls = true,
+            AllowUntrustedCertificates = true
+        };
 
         var options = new MqttClientOptionsBuilder()
             .WithTcpServer("3c6ea0ec32f6404db6fd0439b0d000ce.s2.eu.hivemq.cloud", 8883)
@@ -80,16 +98,7 @@ internal class MQTTClientBackgroundService : BackgroundService<MQTTClientBackgro
             throw new SystemException();
         }
 
-        client.ApplicationMessageReceivedAsync += OnApplicationMessageReceivedFuncAsync;
-
-        await client.SubscribeAsync("encyclopedia/#", MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce, CancellationToken.None);
-
-        while (client.IsConnected)
-        {
-            await client.PingAsync(CancellationToken.None);
-
-            await Task.Delay(1000);
-        }
+        return client;
     }
 
     private async Task OnApplicationMessageReceivedFuncAsync(MqttApplicationMessageReceivedEventArgs eventArgs)
@@ -108,13 +117,28 @@ internal class MQTTClientBackgroundService : BackgroundService<MQTTClientBackgro
             return;
         }
 
-        var deviceSession = await DeviceSessionService.GetOrAddDeviceSessionAsync(model.SessionId);
-        if(deviceSession == default)
+        await HandleModelAsync(model);
+    }
+
+    private async Task HandleModelAsync(ISessionIdentifier? model)
+    {
+        // TODO: Add Throw.IfNull();
+
+        using var scope = ServiceProvider.CreateScope();
+
+        var unitOfWork = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+
+        var deviceSessionService = scope.ServiceProvider.GetRequiredService<IDeviceSessionService>();
+
+        var deviceSession = await deviceSessionService.GetOrAddDeviceSessionAsync(model!.SessionId);
+        if (deviceSession?.Equals(default) ?? true)
         {
             Logger.LogError("Bullshit");
             throw new Exception("Bullshit");
         }
 
         Logger.LogInformation($"Received payload from Session Id {deviceSession.SessionId}");
+
+        unitOfWork.Complete();
     }
 }
