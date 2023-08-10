127e0d6de34d62df2fecc252068f4ad113a7b070
KingoBoiii <kingodk@outlook.dk>
2023-08-10 13:28:37 +0200

    Added ContainerBuilder extensions for registering Unit of Work, SqlClient and Repositories

---
+++ CSharp/MotionController/MotionController.Extensions.Autofac/RepositoryContainerBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System.Reflection;
+using VictorKrogh.Data.Providers;
+using VictorKrogh.Data.Repositories;
+
+namespace VictorKrogh.Extensions.Autofac;
+
+public static class RepositoryContainerBuilderExtensions
+{
+    public static ContainerBuilder RegisterRepositories<TProvider>(this ContainerBuilder containerBuilder) where TProvider : class, IProvider
+    {
+        containerBuilder.RegisterRepositories(typeof(TProvider).Assembly);
+        return containerBuilder;
+    }
+
+    public static ContainerBuilder RegisterRepositories(this ContainerBuilder containerBuilder, Assembly assembly)
+    {
+        containerBuilder.RegisterAssemblyTypes(assembly).AssignableTo<IRepository>().AsImplementedInterfaces();
+        return containerBuilder;
+    }
+}
