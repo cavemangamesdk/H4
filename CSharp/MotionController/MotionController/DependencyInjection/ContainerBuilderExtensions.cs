d46a6444aea928876b46e5e5b3a03a39e69be8ac
KingoBoiii <kingodk@outlook.dk>
2023-08-10 13:34:43 +0200

    Added MotionOptions

--- CSharp/MotionController/MotionController/DependencyInjection/ContainerBuilderExtensions.cs
+++ CSharp/MotionController/MotionController/DependencyInjection/ContainerBuilderExtensions.cs
@@ -1,13 +1,24 @@
 ﻿using Autofac;
 using MotionController.BackgroundServices;
+using MotionController.Db.Data.Providers;
+using MotionController.DependencyInjection;
 using MotionController.Extensions.Autofac;
 
 namespace MotionController.Extensions.DependencyInjection;
 
 public static class ContainerBuilderExtensions
 {
-    public static ContainerBuilder RegisterMotionController(this ContainerBuilder containerBuilder)
+    public static ContainerBuilder RegisterMotionController(this ContainerBuilder containerBuilder, MotionOptions? motionOptions)
     {
+        if(motionOptions == null)
+        {
+            throw new Exception("");
+        }
+
+        containerBuilder.RegisterSqlClientProvider<MotionProvider>(motionOptions.SqlClientProviderSettings);
+
+        containerBuilder.RegisterModule<ServiceModule<MotionOptions>>();
+
         return containerBuilder;
     }
 
