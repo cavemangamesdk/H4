23b3149ac5deb4f700305746b84b64fb4f92d5ee
KingoBoiii <kingodk@outlook.dk>
2023-08-10 13:34:18 +0200

    Added DeviceSessionService

---
+++ CSharp/MotionController/MotionController/Services/DeviceSessionService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Logging;
+using MotionController.Db.Data.Models;
+using MotionController.Db.Data.Repositories;
+
+namespace MotionController.Services;
+
+public interface IDeviceSessionService : IService
+{
+    Task<DeviceSession?> GetDeviceSessionAsync(Guid sessionId);
+    Task<DeviceSession?> AddDeviceSessionAsync(Guid sessionId);
+    Task<DeviceSession?> GetOrAddDeviceSessionAsync(Guid sessionId);
+}
+
+internal class DeviceSessionService : ServiceBase<DeviceSessionService>, IDeviceSessionService
+{
+    public DeviceSessionService(ILogger<DeviceSessionService> logger, IDeviceSessionRepository deviceSessionRepository)
+        : base(logger)
+    {
+        DeviceSessionRepository = deviceSessionRepository;
+    }
+
+    private IDeviceSessionRepository DeviceSessionRepository { get; }
+
+    public async Task<DeviceSession?> GetDeviceSessionAsync(Guid sessionId)
+    {
+        return await DeviceSessionRepository.GetAsync(sessionId);
+    }
+
+    public async Task<DeviceSession?> AddDeviceSessionAsync(Guid sessionId)
+    {
+        var deviceSession = new DeviceSession
+        {
+            SessionId = sessionId
+        };
+
+        var created = await DeviceSessionRepository.AddAsync(deviceSession);
+        if (!created)
+        {
+            return default;
+        }
+        return deviceSession;
+    }
+
+    public async Task<DeviceSession?> GetOrAddDeviceSessionAsync(Guid sessionId)
+    {
+        var deviceSession = await GetDeviceSessionAsync(sessionId);
+        if (deviceSession?.Equals(default) ?? true)
+        {
+            deviceSession = await AddDeviceSessionAsync(sessionId);
+        }
+        return deviceSession;
+    }
+}
