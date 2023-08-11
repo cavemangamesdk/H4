bd5481b6588ccc82d9132aa21af290d368a0a74b
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2023-08-11 09:39:03 +0200

    cleanup, added comments

--- python/motion-controller-project/DataClass/DeviceEnvironmentData.py
+++ python/motion-controller-project/DataClass/DeviceEnvironmentData.py
@@ -3,10 +3,15 @@ from DataClass.DeviceDataBase import DeviceDataBase
 
 @dataclass
 class DeviceEnvironmentDataBase():
+    # from get_temperature()
     temperature: float
+    # from get_temperature_from_humidity()
     temperatureFromHumidity: float
+    # from get_temperature_from_pressure()
     temperatureFromPressure: float
+    # from get_humidity()
     humidity: float
+    # from get_pressure()
     pressure: float
 
 @dataclass
