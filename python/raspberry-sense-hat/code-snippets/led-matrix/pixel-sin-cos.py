1de40af517f88c51cffff61080bcaf30c444451b
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2023-08-11 16:49:16 +0200

    updates

--- python/raspberry-sense-hat/code-snippets/led-matrix/pixel-sin-cos.py
+++ python/raspberry-sense-hat/code-snippets/led-matrix/pixel-sin-cos.py
@@ -1,8 +1,8 @@
-#from sense_hat import SenseHat
+from sense_hat import SenseHat
 import math
 import time
 
-#sense = SenseHat()
+sense = SenseHat()
 
 x = 1
 y = 1
@@ -14,21 +14,12 @@ screen = [[48, 48, 48] for x in range(64)]
 def setPixel(screen, x, y, r, g, b):
     screen[int(x) + int(y) * 8] = [r, g, b]
 
-# Function setPixelFrac that writes to screen variable
-def setPixelFrac(screen, x, y, r, g, b):
-    # Calculate the weights for the neighboring pixels
-    x1, x_frac = divmod(x, 1)
-    y1, y_frac = divmod(y, 1)
-    x1, y1 = int(x1), int(y1)
-    weights = [(1 - x_frac) * (1 - y_frac), x_frac * (1 - y_frac), (1 - x_frac) * y_frac, x_frac * y_frac]
+# function setPixelSmooth that writes to screen variable
+# and uses subpixel rendering
+def setPixelSmooth(screen, x, y, r, g, b):
+    x_frac, x_int = math.modf(x)
+    y_frac, y_int = math.modf(y)
 
-    # Calculate the colors for the neighboring pixels
-    colors = [screen[x1 + dx + (y1 + dy) * 8] for dx, dy in [(0, 0), (1, 0), (0, 1), (1, 1)]]
-    colors = [[int(c + w * color) for c, color in zip(cs, (r, g, b))] for w, cs in zip(weights, colors)]
-
-    # Set the colors of the neighboring pixels
-    for (dx, dy), color in zip([(0, 0), (1, 0), (0, 1), (1, 1)], colors):
-        screen[x1 + dx + (y1 + dy) * 8] = color
 
 while True:
 
@@ -38,19 +29,14 @@ while True:
     x_screen = (3.5 + math.sin(x) * 4)
     y_screen = (3.5 + math.cos(y) * 4)
 
-    # 
+    # Reset the screen
     screen = [[48, 48, 48] for x in range(64)]
 
-    # print(x_screen, y_screen)
+    setPixel(screen, x, y, 0, 0, 255)
 
-    #sense.clear(48, 48, 48)
+    print(screen)
 
-    #sense.set_pixel(x_screen, y_screen, 255, 255, 0)
-
-    setPixelFrac(screen, x, y, 255, 255, 0)
-
-    print(screen) 
+    sense.set_pixels(screen)
 
     time.sleep(1)
     #time.sleep(60/1000)
-
