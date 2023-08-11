110148ef280cab758a672ff9adcf2e2e04aeb263
KingoBoiii <kingodk@outlook.dk>
2023-08-11 12:14:12 +0200

    Enabled swagger UI in production

--- CSharp/MotionController/MotionController Sensor API/Program.cs
+++ CSharp/MotionController/MotionController Sensor API/Program.cs
@@ -29,7 +29,7 @@ builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
