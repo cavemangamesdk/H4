09231edb7301ed810534ba4f4bfaab1396637180
Caveman Games <cavemangames@email.dk>
2023-08-11 15:34:04 +0200

    updates

--- python/raspberry-sense-hat/code-snippets/led-matrix/pixel-sin-cos.py
+++ python/raspberry-sense-hat/code-snippets/led-matrix/pixel-sin-cos.py
@@ -1,25 +1,25 @@
-#from sense_hat import SenseHat
+from sense_hat import SenseHat
 import math
 import time
 
-#sense = SenseHat()
+sense = SenseHat()
 
-x = 1
-y = 1
+x = 0
+y = 0
 
 while True:
 
-    x += 0.11
-    y += 0.11
+    x += 0.07
+    y += 0.07
 
-    x_screen = int(3.5 + math.sin(x) * 4)
-    y_screen = int(3.5 + math.cos(y) * 4)
+    x_screen = round(3.5 + math.sin(x) * 4)
+    y_screen = x_screen #round(3.5 + math.cos(y) * 4)
 
-    print(x_screen, y_screen)
+    # print(x_screen, y_screen)
 
-    #sense.clear(0, 0, 0)
+    sense.clear(48, 48, 48)
 
-    #sense.set_pixel(int(x_screen), int(y_screen), 255, 0, 0)
+    sense.set_pixel(x_screen, y_screen, 255, 255, 0)
 
-    time.sleep(0.1)
+    time.sleep(60/1000)
 
