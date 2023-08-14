9650129378698bdc6731b67ccd765ce30d7710d0
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2023-08-14 07:58:41 +0200

    updates

--- python/motion-controller-project/DataClass/DeviceAllData.py
+++ python/motion-controller-project/DataClass/DeviceAllData.py
@@ -13,31 +13,7 @@ class Data():
 @dataclass
 class DeviceAllDataBase():
     data: Data.__dict__
-    # env: DeviceEnvDataBase.__dict__
-    # imu: DeviceImuDataBase.__dict__
 
 @dataclass
 class DeviceAllData(DeviceDataBase, DeviceAllDataBase):
     pass
-
-# from dataclasses import dataclass
-
-# from DataClass.DeviceDataBase import DeviceDataBase
-# from DataClass.DeviceAccelerometerData import DeviceAccelerometerDataBase
-# from DataClass.DeviceEnvironmentData import DeviceEnvironmentDataBase
-# from DataClass.DeviceGyroscopeData import DeviceGyroscopeDataBase
-# from DataClass.DeviceMagnetometerData import DeviceMagnetometerDataBase
-# from DataClass.DeviceOrientationData import DeviceOrientationDataBase
-
-# # dataclass for all the data from the SenseHat
-# @dataclass
-# class DeviceAllDataBase():
-#     accelerometer: DeviceAccelerometerDataBase.__dict__
-#     environment: DeviceEnvironmentDataBase.__dict__
-#     gyroscope: DeviceGyroscopeDataBase.__dict__
-#     magnetometer: DeviceMagnetometerDataBase.__dict__
-#     orientation: DeviceOrientationDataBase.__dict__
-
-# @dataclass
-# class DeviceAllData(DeviceDataBase, DeviceAllDataBase):
-#     pass
--- python/motion-controller-project/test.py
+++ python/motion-controller-project/test.py
@@ -3,17 +3,14 @@ import get_data
 import datetime
 import uuid
 
-# Sense Hat env data
+#
 sense = SenseHat()
-
-# Disable and re-enable all IMU sensors in order to reset them
-sense.set_imu_config(False, False, False)  # Disable all sensors
-sense.set_imu_config(True, True, True)  # Enable all sensors
-
 uuidDevice = uuid.uuid4()
-
 dateTime = datetime
 
-all_data = get_data.getAllData(sense, uuidDevice, dateTime) 
+# Test all get data functions
+#data = get_data.getAllDataBase(sense)
+#data = get_data.getAllData(sense, uuidDevice, dateTime)
+data = get_data.getEnvDataBase(sense) 
 
-print(all_data)
+print(data)
