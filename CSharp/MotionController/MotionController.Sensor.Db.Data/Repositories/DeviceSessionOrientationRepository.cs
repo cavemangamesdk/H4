48149e96cbcf1cd4a7ad28434cdb08a6f3a0ed05
KingoBoiii <kingodk@outlook.dk>
2023-08-15 11:12:09 +0200

    Added DeviceOrientation message handler, model and repository

---
+++ CSharp/MotionController/MotionController.Sensor.Db.Data/Repositories/DeviceSessionOrientationRepository.cs
@@ -0,0 +1,27 @@
+﻿using MotionController.Data.Repositories;
+using MotionController.Data.Repositories.Database;
+using MotionController.Db.Data.Providers;
+using MotionController.Sensor.Db.Data.Models;
+
+namespace MotionController.Db.Data.Repositories;
+
+public interface IDeviceSessionOrientationRepository : IRepository<DeviceSessionOrientation, int>
+{
+}
+
+internal class DeviceSessionOrientationRepository : DbRepositoryBase<DeviceSessionOrientation, int>, IDeviceSessionOrientationRepository
+{
+    public DeviceSessionOrientationRepository(IMotionProvider provider)
+        : base(provider)
+    {
+    }
+
+    public override Task<bool> AddAsync(DeviceSessionOrientation model)
+    {
+        model.Created = DateTime.Now;
+        model.Modified = DateTime.Now;
+
+        return base.AddAsync(model);
+    }
+}
+
---
+++ CSharp/MotionController/MotionController.Sensor/Services/DeviceSessionOrientationService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using MotionController.Db.Data.Repositories;
+using MotionController.Sensor.Db.Data.Models;
+
+namespace MotionController.Services;
+
+public interface IDeviceSessionOrientationService : IService
+{
+    Task<bool> AddDeviceSessionOrientationAsync(DeviceSessionOrientation deviceSessionOrientation);
+}
+
+internal class DeviceSessionOrientationService : ServiceBase<DeviceSessionOrientationService>, IDeviceSessionOrientationService
+{
+    public DeviceSessionOrientationService(ILogger<DeviceSessionOrientationService> logger, IDeviceSessionOrientationRepository deviceSessionOrientationRepository)
+        : base(logger)
+    {
+        DeviceSessionOrientationRepository = deviceSessionOrientationRepository;
+    }
+
+    private IDeviceSessionOrientationRepository DeviceSessionOrientationRepository { get; }
+
+    public async Task<bool> AddDeviceSessionOrientationAsync(DeviceSessionOrientation deviceSessionOrientation)
+    {
+        return await DeviceSessionOrientationRepository.AddAsync(deviceSessionOrientation);
+    }
+}
