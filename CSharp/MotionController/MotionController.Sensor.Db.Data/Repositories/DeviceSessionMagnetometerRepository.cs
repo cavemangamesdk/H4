9be245db93ea5ed90b63c95e26c1e52ad7a84768
KingoBoiii <kingodk@outlook.dk>
2023-08-15 11:15:33 +0200

    Added Magnetometer message hander, model and repository

---
+++ CSharp/MotionController/MotionController.Sensor.Db.Data/Repositories/DeviceSessionMagnetometerRepository.cs
@@ -0,0 +1,27 @@
+﻿using MotionController.Data.Repositories;
+using MotionController.Data.Repositories.Database;
+using MotionController.Db.Data.Providers;
+using MotionController.Sensor.Db.Data.Models;
+
+namespace MotionController.Db.Data.Repositories;
+
+public interface IDeviceSessionMagnetometerRepository : IRepository<DeviceSessionMagnetometer, int>
+{
+}
+
+internal class DeviceSessionMagnetometerRepository : DbRepositoryBase<DeviceSessionMagnetometer, int>, IDeviceSessionMagnetometerRepository
+{
+    public DeviceSessionMagnetometerRepository(IMotionProvider provider)
+        : base(provider)
+    {
+    }
+
+    public override Task<bool> AddAsync(DeviceSessionMagnetometer model)
+    {
+        model.Created = DateTime.Now;
+        model.Modified = DateTime.Now;
+
+        return base.AddAsync(model);
+    }
+}
+
