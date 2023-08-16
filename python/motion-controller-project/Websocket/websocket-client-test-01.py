dcd5c4f9ceeca2c3dad615131d2db6eff08ae380
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2023-08-16 10:35:11 +0200

    updates

--- python/motion-controller-project/Websocket/websocket-client-test-01.py
+++ python/motion-controller-project/Websocket/websocket-client-test-01.py
@@ -1,12 +1,9 @@
 import asyncio
 import websockets
 
-data = 'Message received: sensehat/imu/orientation 1 b{"session_id": "480cd0ac-2379-4fa4-a889-78b6cecc1835", "timestamp": "2023-08-16 09:08:34.122261", "data": {"roll_deg": 23.67335028589957, "pitch_deg": 6.138737957594272, "yaw_deg": 111.19384044240678, "roll_rad": 0.4131779074668884, "pitch_rad": 0.10714118182659149, "yaw_rad": 1.940916895866394}}'
-
 async def hello():
     async with websockets.connect("ws://192.168.109.110:8765") as websocket:
-        #await websocket.send("Hello world!")
-        await websocket.send(data)
+        await websocket.send("Hello world!")
         message = await websocket.recv()
         print(f"Received: {message}")
 
--- python/motion-controller-project/Websocket/websocket-server-test-02.py
+++ python/motion-controller-project/Websocket/websocket-server-test-02.py
@@ -1,10 +1,16 @@
 import asyncio
 import websockets
 
+async def get_data():
+    # This is where you would put your logic to get the data
+    # For now, I'll just return a string
+    return "This is the data to be sent to the client."
+
 async def echo(websocket, path):
-    async for message in websocket:
-        #await websocket.send(message)
-        print(f"Received: {message}")
+    while True:
+        data = await get_data()
+        await websocket.send(data)
+        await asyncio.sleep(0.1)  # sleep for 0.1 seconds
 
 start_server = websockets.serve(echo, "192.168.109.110", 8765)
 
