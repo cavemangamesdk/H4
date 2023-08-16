b00ea795e304a70ca9bf1154fe966a77ca73a393
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2023-08-16 10:08:03 +0200

    websocket server

--- python/motion-controller-project/Websocket/websocket-client-test-01.py
+++ python/motion-controller-project/Websocket/websocket-client-test-01.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 
 async def hello():
-    async with websockets.connect("ws://192.168.109.110:8764") as websocket:
+    async with websockets.connect("ws://192.168.109.110:8765") as websocket:
         await websocket.send("Hello world!")
         message = await websocket.recv()
         print(f"Received: {message}")
