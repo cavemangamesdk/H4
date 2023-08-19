88551b327cb131d2113ade84bdf667b99309130b
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2023-08-19 12:27:57 +0200

    fixed import bug

--- python/motion-controller-project/BNO055/BNO055-test-01.py
+++ python/motion-controller-project/BNO055/BNO055-test-01.py
@@ -3,12 +3,12 @@
 
 import time
 import board
-import adafruit_bno055
+import Adafruit_BNO055
 
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-sensor = adafruit_bno055.BNO055_I2C(i2c)
+sensor = Adafruit_BNO055.BNO055_I2C(i2c)
 
 # If you are going to use UART uncomment these lines
 # uart = board.UART()
