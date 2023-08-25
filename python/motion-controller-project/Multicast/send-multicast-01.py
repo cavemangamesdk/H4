08389f69936029199bd8572b89f6fb967dfb4b30
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2023-08-25 10:46:42 +0200

    updates

--- python/motion-controller-project/Multicast/send-multicast-01.py
+++ python/motion-controller-project/Multicast/send-multicast-01.py
@@ -1,11 +1,25 @@
 import socket
-import psutil
+import struct
+import time
 
-def get_local_ip_address(interface):
-    interface_addrs = psutil.net_if_addrs().get(interface) or []
-    for snicaddr in interface_addrs:
-            if snicaddr.family == socket.AF_INET:
-                    return snicaddr.address
-    #return False
+message = 'very important data'
+multicast_group = ('224.1.1.1', 58008)
 
-print(get_local_ip_address("wlan0"))
+# Create the datagram socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Set a timeout to avoid blocking indefinitely
+sock.settimeout(0.2)
+
+# Set the time-to-live for messages to 1
+ttl = struct.pack('b', 1)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+# Send data to the multicast group
+for i in range(1, 10):
+    print(f'sending {i} "%s"' % message)
+    sent = sock.sendto(message.encode(), multicast_group)
+    time.sleep(1)
+
+# Close the socket
+sock.close()
