759148f74e9740f2d09216b74837478cfd4d449c
KingoBoiii <kingodk@outlook.dk>
2023-08-25 09:56:34 +0200

    Refactored MqttClient creation - now done by IoC container

---
+++ CSharp/MotionController/MotionController.MQTT/DependencyInjection/ContainerBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using MQTTnet;
+using MQTTnet.Client;
+
+namespace MotionController.Extensions.DependencyInjection;
+
+public static class ContainerBuilderExtensions
+{
+    public static ContainerBuilder RegisterMQTT(this ContainerBuilder containerBuilder)
+    {
+        containerBuilder.RegisterType<MqttFactory>()
+            .AsSelf()
+            .SingleInstance();
+
+        return containerBuilder;
+    }
+
+    public static ContainerBuilder RegisterMQTTClient(this ContainerBuilder containerBuilder)
+    {
+        containerBuilder.Register((cc, p) =>
+        {
+            var mqttFactory = cc.Resolve<MqttFactory>();
+
+            return mqttFactory.CreateMqttClient();
+        })
+            .As<IMqttClient>()
+            .SingleInstance();
+
+        return containerBuilder;
+    }
+}
