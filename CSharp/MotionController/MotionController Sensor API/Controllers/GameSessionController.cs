31ccbcae95e110fbd3cf75b4a9ce643c5119b814
KingoBoiii <kingodk@outlook.dk>
2023-08-28 15:09:23 +0200

    Added ugly test code

--- CSharp/MotionController/MotionController Sensor API/Controllers/GameSessionController.cs
+++ CSharp/MotionController/MotionController Sensor API/Controllers/GameSessionController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using NSwag.Annotations;
 using System.Globalization;
+using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace MotionController.API.Controllers
 {
@@ -19,15 +22,11 @@ namespace MotionController.API.Controllers
         [Route("", Name = nameof(AddGameSessionAsync))]
         [OpenApiOperation(nameof(AddGameSessionAsync), "Adds a Game Session", "")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> AddGameSessionAsync()
+        public async Task<IActionResult> AddGameSessionAsync([FromBody] GameSession gameSession)
         {
             try
             {
-                var timeStr = "01:28:31";
-
-                var time = TimeSpan.Parse(timeStr);
-
-                var timeExact = TimeSpan.ParseExact(timeStr, @"m\:s\:ff", CultureInfo.InvariantCulture);
+                Logger.LogError("sdfogfgjiog", gameSession);
 
                 return await Task.FromResult(Ok());
             }
@@ -37,5 +36,62 @@ namespace MotionController.API.Controllers
                 throw;
             }
         }
+    }
+
+    public class GameSession
+    {
+        [JsonProperty("Guid")]
+        public Guid Guid { get; set; }
+
+        [JsonProperty("PlayerData")]
+        public PlayerData? PlayerData { get; set; }
+
+        [JsonProperty("GameData")]
+        public GameData[] GameData { get; set; } = Array.Empty<GameData>();
+    }
+
+    public class PlayerData
+    {
+        [JsonProperty("Name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("Lives")]
+        public int Lives { get; set; }
+
+        [JsonProperty("GameTime")]
+        public string? GameTime { get; set; }
+    }
+
+    public class GameData
+    {
+        [JsonProperty("BallPosition")]
+        public Vector3? BallPosition { get; set; }
+
+        [JsonProperty("BoardRotation")]
+        public Vector3? BoardRotation { get; set; }
+
+        [JsonProperty("InputData")]
+        public Vector2? InputData { get; set; }
+    }
+
+    public class Vector3
+    {
+        [JsonProperty("x")]
+        public float X { get; set; }
+
+        [JsonProperty("y")]
+        public float Y { get; set; }
+
+        [JsonProperty("z")]
+        public float Z { get; set; }
+    }
+
+    public class Vector2
+    {
+        [JsonProperty("x")]
+        public float X { get; set; }
+
+        [JsonProperty("y")]
+        public float Y { get; set; }
     }
 }
