873785d6d7dd3994792abfaf3261d2a23881fef9
KingoBoiii <kingodk@outlook.dk>
2023-08-29 09:50:07 +0200

    Added logic for saving GameSession

--- CSharp/MotionController/MotionController.Sensor/Services/GameSessionService.cs
+++ CSharp/MotionController/MotionController.Sensor/Services/GameSessionService.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
+using MotionController.Sensor.Db.Data.Models;
+using MotionController.Sensor.Db.Data.Repositories;
 using MotionController.Sensor.Models.Game;
 using MotionController.Services;
+using System.Globalization;
 
 namespace MotionController.Sensor.Services;
 
@@ -11,14 +14,36 @@ public interface IGameSessionService : IService
 
 internal class GameSessionService : ServiceBase<GameSessionService>, IGameSessionService
 {
-    public GameSessionService(ILogger<GameSessionService> logger) 
+    private const string GameTimeFormat = @"m\:s\:ff";
+
+    public GameSessionService(ILogger<GameSessionService> logger, IGameSessionRepository gameSessionRepository) 
         : base(logger)
     {
+        GameSessionRepository = gameSessionRepository;
     }
 
-    public Task<bool> AddGameSessionAsync(GameSession gameSession)
-    public Task<bool> AddGameSessionAsync(UnityGameSession gameSession)
+    private IGameSessionRepository GameSessionRepository { get; }
+
+    public async Task<bool> AddGameSessionAsync(UnityGameSession unityGameSession)
     {
-        throw new NotImplementedException();
+        if(!TimeSpan.TryParseExact(unityGameSession?.PlayerData?.GameTime ?? string.Empty, GameTimeFormat, CultureInfo.InvariantCulture, out var gameTimeSpan))
+        {
+            return false;
+        }
+
+        if(unityGameSession == default)
+        {
+            return false;
+        }
+
+        var gameSession = new GameSession
+        {
+            SessionId = unityGameSession.Guid,
+            PlayerName = unityGameSession.PlayerData?.Name ?? string.Empty,
+            Lives = unityGameSession.PlayerData?.Lives ?? default,
+            GameTime = gameTimeSpan
+        };
+
+        return await GameSessionRepository.AddAsync(gameSession);
     }
 }
