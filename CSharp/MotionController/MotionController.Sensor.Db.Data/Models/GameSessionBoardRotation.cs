0ffb4ec8008192fbc9f2046e29f4292f52fdd9c6
KingoBoiii <kingodk@outlook.dk>
2023-08-29 09:33:39 +0200

    Added database models for GameSession, BallPosition, BoardRotation and InputData

---
+++ CSharp/MotionController/MotionController.Sensor.Db.Data/Models/GameSessionBoardRotation.cs
@@ -0,0 +1,33 @@
+﻿using MotionController.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MotionController.Sensor.Db.Data.Models;
+
+[Schema("H4")]
+[Table("GameSessionBoardRotation")]
+public sealed class GameSessionBoardRotation : DatabaseModel
+{
+    [Key]
+    [Column("Id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Column("GameSession_Id")]
+    public int GameSessionId { get; set; }
+
+    [Column("X")]
+    public decimal X { get; set; }
+
+    [Column("Y")]
+    public decimal Y { get; set; }
+
+    [Column("Z")]
+    public decimal Z { get; set; }
+
+    [Column("Created")]
+    public DateTime Created { get; set; }
+
+    [Column("Modified")]
+    public DateTime Modified { get; set; }
+}
