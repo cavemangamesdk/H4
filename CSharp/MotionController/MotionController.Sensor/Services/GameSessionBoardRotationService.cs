d0e0dbc2f09d9cdc22bfb226ee731db158e6f9c2
KingoBoiii <kingodk@outlook.dk>
2023-08-29 13:33:03 +0200

    Added BoardRotation and Inputdata Service

---
+++ CSharp/MotionController/MotionController.Sensor/Services/GameSessionBoardRotationService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using MotionController.Sensor.Db.Data.Models;
+using MotionController.Sensor.Db.Data.Repositories;
+using MotionController.Sensor.Models.Game;
+using MotionController.Services;
+
+namespace MotionController.Sensor.Services;
+
+public interface IGameSessionBoardRotationService : IService
+{
+    Task<bool> AddGameSessionBoardRotationAsync(GameSession gameSession, Vector3 boardRotation);
+}
+
+internal class GameSessionBoardRotationService : ServiceBase<GameSessionBoardRotationService>, IGameSessionBoardRotationService
+{
+    public GameSessionBoardRotationService(ILogger<GameSessionBoardRotationService> logger, IGameSessionBoardRotationRepository gameSessionBoardRotationRepository) 
+        : base(logger)
+    {
+        GameSessionBoardRotationRepository = gameSessionBoardRotationRepository;
+    }
+
+    private IGameSessionBoardRotationRepository GameSessionBoardRotationRepository { get; }
+
+    public async Task<bool> AddGameSessionBoardRotationAsync(GameSession gameSession, Vector3 boardRotation)
+    {
+        if (boardRotation == default)
+        {
+            return false;
+        }
+
+        var gameSessionBoardRotation = new GameSessionBoardRotation
+        {
+            GameSessionId = gameSession.Id,
+            X = boardRotation.X,
+            Y = boardRotation.Y,
+            Z = boardRotation.Z
+        };
+
+        return await GameSessionBoardRotationRepository.AddAsync(gameSessionBoardRotation);
+    }
+}
