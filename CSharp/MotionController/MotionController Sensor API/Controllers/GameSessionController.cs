5e97c87e6b491fb32dace6692b01aa93abe5a79c
KingoBoiii <kingodk@outlook.dk>
2023-08-31 12:15:39 +0200

    Using file scoped namespaces

--- CSharp/MotionController/MotionController Sensor API/Controllers/GameSessionController.cs
+++ CSharp/MotionController/MotionController Sensor API/Controllers/GameSessionController.cs
@@ -4,93 +4,92 @@ using MotionController.Sensor.Models.Game;
 using MotionController.Sensor.Services;
 using NSwag.Annotations;
 
-namespace MotionController.API.Controllers
+namespace MotionController.API.Controllers;
+
+[Route("api/v1/game/sessions")]
+[OpenApiController("GameSession")]
+public class GameSessionController : ControllerBase
 {
-    [Route("api/v1/game/sessions")]
-    [OpenApiController("GameSession")]
-    public class GameSessionController : ControllerBase
+    public GameSessionController(ILogger<GameSessionController> logger, IGameSessionService gameSessionService)
     {
-        public GameSessionController(ILogger<GameSessionController> logger, IGameSessionService gameSessionService)
+        Logger = logger;
+        GameSessionService = gameSessionService;
+    }
+
+    private ILogger<GameSessionController> Logger { get; }
+    private IGameSessionService GameSessionService { get; }
+
+    [HttpGet]
+    [Route("", Name = nameof(GetGameSessionsAsync))]
+    [OpenApiOperation(nameof(GetGameSessionsAsync), "Gets all Game Sessions", "")]
+    [ProducesResponseType(typeof(IEnumerable<GameSession?>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetGameSessionsAsync()
+    {
+        try
         {
-            Logger = logger;
-            GameSessionService = gameSessionService;
+            var gameSessions = await GameSessionService.GetGameSessionsAsync();
+            if (!gameSessions.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(gameSessions);
         }
-
-        private ILogger<GameSessionController> Logger { get; }
-        private IGameSessionService GameSessionService { get; }
-
-        [HttpGet]
-        [Route("", Name = nameof(GetGameSessionsAsync))]
-        [OpenApiOperation(nameof(GetGameSessionsAsync), "Gets all Game Sessions", "")]
-        [ProducesResponseType(typeof(IEnumerable<GameSession?>), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetGameSessionsAsync()
+        catch (Exception ex)
         {
-            try
-            {
-                var gameSessions = await GameSessionService.GetGameSessionsAsync();
-                if (!gameSessions.Any())
-                {
-                    return NotFound();
-                }
-
-                return Ok(gameSessions);
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex, $"{nameof(GetGameSessionsAsync)} operation failed.");
-                throw;
-            }
+            Logger.LogError(ex, $"{nameof(GetGameSessionsAsync)} operation failed.");
+            throw;
         }
+    }
 
-        [HttpGet]
-        [Route("{sessionId:guid}", Name = nameof(GetGameSessionBySessionIdAsync))]
-        [OpenApiOperation(nameof(GetGameSessionBySessionIdAsync), "Get a Game Session by Session Id", "")]
-        [ProducesResponseType(typeof(GameSession), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetGameSessionBySessionIdAsync([FromRoute] Guid sessionId)
+    [HttpGet]
+    [Route("{sessionId:guid}", Name = nameof(GetGameSessionBySessionIdAsync))]
+    [OpenApiOperation(nameof(GetGameSessionBySessionIdAsync), "Get a Game Session by Session Id", "")]
+    [ProducesResponseType(typeof(GameSession), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetGameSessionBySessionIdAsync([FromRoute] Guid sessionId)
+    {
+        try
         {
-            try
+            var gameSession = await GameSessionService.GetGameSessionAsync(sessionId);
+            if (gameSession?.Equals(default) ?? true)
             {
-                var gameSession = await GameSessionService.GetGameSessionAsync(sessionId);
-                if (gameSession?.Equals(default) ?? true)
-                {
-                    return NotFound();
-                }
+                return NotFound();
+            }
 
-                return Ok(gameSession);
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex, $"{nameof(GetGameSessionBySessionIdAsync)} operation failed.");
-                throw;
-            }
+            return Ok(gameSession);
         }
-
-        [HttpPost]
-        [Route("", Name = nameof(AddGameSessionAsync))]
-        [OpenApiOperation(nameof(AddGameSessionAsync), "Adds a Game Session", "")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> AddGameSessionAsync([FromBody] UnityGameSession unityGameSession)
+        catch (Exception ex)
         {
-            try
-            {
-                var created = await GameSessionService.CreateGameSessionAsync(unityGameSession);
-                if (created)
-                {
-                    //var gameSession = await GameSessionService.GetGameSessionAsync(unityGameSession.Guid);
-                    //return CreatedAtRoute(nameof(GetGameSessionBySessionIdAsync), new { sessionId = unityGameSession.Guid }, gameSession);
+            Logger.LogError(ex, $"{nameof(GetGameSessionBySessionIdAsync)} operation failed.");
+            throw;
+        }
+    }
 
-                    return Ok();
-                }
-
-                return BadRequest();
-            }
-            catch (Exception ex)
+    [HttpPost]
+    [Route("", Name = nameof(AddGameSessionAsync))]
+    [OpenApiOperation(nameof(AddGameSessionAsync), "Adds a Game Session", "")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> AddGameSessionAsync([FromBody] UnityGameSession unityGameSession)
+    {
+        try
+        {
+            var created = await GameSessionService.CreateGameSessionAsync(unityGameSession);
+            if (created)
             {
-                Logger.LogError(ex, $"{nameof(AddGameSessionAsync)} operation failed.");
-                throw;
+                //var gameSession = await GameSessionService.GetGameSessionAsync(unityGameSession.Guid);
+                //return CreatedAtRoute(nameof(GetGameSessionBySessionIdAsync), new { sessionId = unityGameSession.Guid }, gameSession);
+
+                return Ok();
             }
+
+            return BadRequest();
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, $"{nameof(AddGameSessionAsync)} operation failed.");
+            throw;
         }
     }
 }
