c072ce3410ea3ecc3aa639ab48d35a02827d763d
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2023-09-07 17:22:28 +0200

    code cleanup

--- C++/Raspberry-Cpp-Sense-Hat-Motion-Controller/sense-hat-controller-02.cpp
+++ C++/Raspberry-Cpp-Sense-Hat-Motion-Controller/sense-hat-controller-02.cpp
@@ -1,6 +1,5 @@
-/*
-* Source: https://github.com/platu/libsensehat-cpp
-*/
+// sensehat library: https://github.com/platu/libsensehat-cpp
+// boost library: https://www.boost.org/
 
 #include <boost/asio.hpp>
 #include <sensehat.h>
@@ -31,42 +30,40 @@ string GetOrientationData() {
 }
 
 joystickState GetJoystickInput() {
+    
     stick_t joystick;
+    joystickState joystickState;
+
     senseSetJoystickWaitTime(0, 20);
 	bool clicked = senseGetJoystickEvent(joystick);
+    
     if (clicked) {
 
-        joystickState joystickState;
-
-        // Identify action on stick
         switch (joystick.action) {
             case KEY_ENTER:	joystickState.action = "push  "; break;
             case KEY_UP:	joystickState.action = "up    "; break;
             case KEY_LEFT:	joystickState.action = "left  "; break;
             case KEY_RIGHT:	joystickState.action = "right "; break;
             case KEY_DOWN:	joystickState.action = "down  "; break;
-            // case else
             default:		joystickState.action = "none  "; break;
         }
 
-        // Identify state of stick
         switch(joystick.state) {
             case KEY_RELEASED:	joystickState.state = "released"; break;
             case KEY_PRESSED:	joystickState.state = "pressed"; break;
             case KEY_HELD:		joystickState.state = "held"; break;
-            // case else
             default:			joystickState.state = "none"; break;
         }
 
         return joystickState;
 
     } else {
-        joystickState joystickState;
-        joystickState.action = "none  ";
+
+        joystickState.action = "none";
         joystickState.state = "none";
+
         return joystickState;
     }
-    //sleep_until(system_clock::now() + milliseconds(20));
 }
 
 int main() {
@@ -75,7 +72,6 @@ int main() {
     std::string orientation_port = "5100";
     std::string joystick_port = "5101";
 
-    
     io_service io_service;
 
     ip::udp::resolver resolver(io_service);
@@ -87,9 +83,12 @@ int main() {
     ip::udp::endpoint joystick_endpoint = *resolver.resolve(joystickQuery);
 
     ip::udp::socket socket(io_service);
+
     socket.open(ip::udp::v4());
 
     if(ConnectSenseHat()) {
+
+        // TODO: exit program if joystick is clicked?
         while(true) {
             
             string orientationMessage = GetOrientationData();
@@ -97,11 +96,7 @@ int main() {
 
             socket.send_to(buffer(orientationMessage, orientationMessage.size()), orientation_endpoint);
             socket.send_to(buffer(joystickEvent.action + "," + joystickEvent.state, joystickEvent.action.size() + joystickEvent.state.size()), joystick_endpoint);
-
-            //socket.send_to(buffer(to_string(joystickEvent.timestamp) + "," + to_string(joystickEvent.type) + "," + to_string(joystickEvent.code) + "," + to_string(joystickEvent.value), 100), joystick_endpoint);
-            
-            // Adding sleep for a while, to not overwhelm the network or CPU. 
-            // You can modify this sleep duration or remove it as per your requirement.
+  
             this_thread::sleep_for(std::chrono::milliseconds(10));
         }
     }
