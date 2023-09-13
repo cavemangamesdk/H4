15799230067381d37415da3775d7ce660a366dd0
h4tt3n <michaelschmidtnissen@gmail.com>
2023-09-13 15:47:08 +0200

    raylib controller test code

--- raylib/raylib-test-02.cpp
+++ raylib/raylib-test-02.cpp
@@ -49,51 +49,71 @@ int main() {
     SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
+    // String delimiter
+    std::string delimiter = ",";
 
     while (!WindowShouldClose()) {
         
-
-        UpdateCamera(&camera, CAMERA_FREE);
-        camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-        
-        
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        
-
-        BeginMode3D(camera);
-
-            DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-            DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-
-            DrawGrid(10, 1.0f);
-
-        EndMode3D();
-
-        // DrawRectangle( 10, 10, 320, 133, Fade(SKYBLUE, 0.5f));
-        // DrawRectangleLines( 10, 10, 320, 133, BLUE);
-
-        DrawText("UDP Receiver", 10, 10, 20, DARKGRAY);
-
+        // Receive UDP data from game controller
         boost::asio::ip::udp::endpoint sender_endpoint;
         size_t length;
 
         // Receive from socket1
         length = socket1.receive_from(boost::asio::buffer(buffer), sender_endpoint);
-        std::string received_data(buffer.data(), length);
-        std::cout << "Received data from " << sender_endpoint.address() << ":" << sender_endpoint.port()
-                  << " (Port 5100): " << received_data << std::endl;
-        outputFile << "5100," << received_data << std::endl;
-        DrawText(received_data.c_str(), 10, 40, 20, BLACK);
+        std::string orientation_data(buffer.data(), length);
+
+        std::string x = orientation_data.substr(0, orientation_data.find(delimiter));
+        std::string y = orientation_data.substr(1, orientation_data.find(delimiter));
+
+        std::cout << x << ", " << y << std::endl;
+
+        // std::cout << "Received data from " << sender_endpoint.address() << ":" << sender_endpoint.port()
+        //         << " (Port 5100): " << orientation_data << std::endl;
+        //outputFile << "5100," << orientation_data << std::endl;
 
         // Receive from socket2
         length = socket2.receive_from(boost::asio::buffer(buffer), sender_endpoint);
-        received_data = std::string(buffer.data(), length);
-        std::cout << "Received data from " << sender_endpoint.address() << ":" << sender_endpoint.port()
-                  << " (Port 5101): " << received_data << std::endl;
-        outputFile << "5101," << received_data << std::endl;
-        DrawText(received_data.c_str(), 10, 70, 20, BLACK);
+        std::string joystick_data = std::string(buffer.data(), length);
+
+        std::string action = joystick_data.substr(0, joystick_data.find(delimiter));
+        std::string state = joystick_data.substr(1, joystick_data.find(delimiter));
+
+        std::cout << action << ", " << state << std::endl;
+
+        // std::cout << "Received data from " << sender_endpoint.address() << ":" << sender_endpoint.port()
+        //         << " (Port 5101): " << joystick_data << std::endl;
+        //outputFile << "5101," << joystick_data << std::endl;
+    
+        
+        UpdateCamera(&camera, CAMERA_FREE);
+        
+        camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+        
+        // joystick_data case
+        if(action == "up") {
+            camera.fovy += 1.0f;
+        }
+
+        if(action == "down") {
+            camera.fovy -= 1.0f;
+        }
+        
+        BeginDrawing();
+            
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+                DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+                DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+                DrawGrid(10, 1.0f);
+
+            EndMode3D();
+
+            DrawText("UDP Receiver", 10, 10, 20, DARKGRAY);
+            DrawText(orientation_data.c_str(), 10, 40, 20, BLACK);
+            DrawText(joystick_data.c_str(), 10, 70, 20, BLACK);
 
         EndDrawing();
     }
