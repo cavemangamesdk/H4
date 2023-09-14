42178180f25582d27c76ac2682cbd19fe9082ecd
h4tt3n <michaelschmidtnissen@gmail.com>
2023-09-14 22:31:28 +0200

    Fucking ball in a maze!!!

--- bullet/bullet-raylib-test-02.cpp
+++ bullet/bullet-raylib-test-02.cpp
@@ -2,8 +2,49 @@
 #include <btBulletDynamicsCommon.h>
 #include <rlgl.h>
 #include <raymath.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <boost/asio.hpp>  // required for UDP
+#include <ctime>
+
+// for string delimiter
+std::vector<std::string> split(std::string s, std::string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    std::string token;
+    std::vector<std::string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back (token);
+    }
+
+    res.push_back (s.substr (pos_start));
+    return res;
+}
 
 int main(int argc, char** argv) {
+
+    // UDP  
+    const std::string IP_ADDR = "192.168.109.243";
+
+    // String delimiter
+    std::string delimiter = ",";
+
+    boost::asio::io_context io_context;
+    boost::asio::ip::udp::socket socket1(io_context);
+
+    // Bind the first socket to port 5100
+    boost::asio::ip::udp::endpoint receiver_endpoint1(boost::asio::ip::make_address(IP_ADDR), 5100);
+    socket1.open(boost::asio::ip::udp::v4());
+    socket1.bind(receiver_endpoint1);
+
+    std::array<char, 1024> buffer;
+
+    float pitch = 0.0f;
+    float roll = 0.0f;
+    float yaw = 0.0f;
 
     // Initialize bullet physics
 	///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
@@ -15,7 +56,7 @@ int main(int argc, char** argv) {
 	///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
 	btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
 	btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
-	dynamicsWorld->setGravity(btVector3(0, -10, 0));
+	dynamicsWorld->setGravity(btVector3(0, -100, 0));
 
     // Create tile physics body
     btCollisionShape* tileShape = new btBoxShape(btVector3(10, 1, 10)); // Change the size to make it a wide, square, tile shape
@@ -28,7 +69,7 @@ int main(int argc, char** argv) {
     dynamicsWorld->addRigidBody(tileRigidBody);
 
     // Create sphere physics body
-    btCollisionShape* sphereShape = new btSphereShape(1);
+    btCollisionShape* sphereShape = new btSphereShape(0.5);
     btDefaultMotionState* sphereMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 10, 0))); // Set the initial position to be right above the tile
     btScalar sphereMass = 1;
     btVector3 sphereInertia(0, 0, 0);
@@ -49,36 +90,58 @@ int main(int argc, char** argv) {
     tileRigidBody->getMotionState()->getWorldTransform(tileTransform);
     btVector3 tileOrigin = tileTransform.getOrigin();
     camera.target = (Vector3){tileOrigin.getX(), tileOrigin.getY(), tileOrigin.getZ()};  // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.up = (Vector3){ 0.0f, 2.0f, 0.0f };          // Camera up vector (rotation towards target)
     camera.fovy = 45.0f;                                // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;                   // Camera mode type
 
+    Mesh cubeMesh = GenMeshCube(20.0f, 1.0f, 20.0f);
+    Model cubeModel = LoadModelFromMesh(cubeMesh);
 
     //SetCameraMode(camera, CAMERA_FREE); // Set a free camera mode
 
     SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
 
-    btScalar rotationAngleX = 0.0f; // Set this to the desired rotation angle around the x axis, in radians
-    btVector3 rotationAxisX(1, 0, 0); // x axis
-    btQuaternion rotationX(rotationAxisX, rotationAngleX);
 
-    btScalar rotationAngleZ = 0.05f; // Set this to the desired rotation angle around the z axis, in radians
-    btVector3 rotationAxisZ(0, 0, 1); // z axis
-    btQuaternion rotationZ(rotationAxisZ, rotationAngleZ);
-
-    btQuaternion rotation = rotationX * rotationZ;
-
-    //btTransform tileTransform;
-    tileRigidBody->getMotionState()->getWorldTransform(tileTransform);
-    tileTransform.setRotation(rotation);
-    tileRigidBody->setWorldTransform(tileTransform);
 
     // Game loop
     while (!WindowShouldClose())
     {
 
+        // UDP
+
+        // Receive UDP data from game controller
+        boost::asio::ip::udp::endpoint sender_endpoint;
+        size_t length;
+
+        // Receive from socket1
+        length = socket1.receive_from(boost::asio::buffer(buffer), sender_endpoint);
+        std::string orientation_data(buffer.data(), length);
+
+        // Split string
+        std::vector<std::string> orientation = split(orientation_data, delimiter);
+        std::string orientation_x = orientation[0];
+        std::string orientation_y = orientation[1];
+
+        pitch = std::stof(orientation_x)*DEG2RAD;
+        roll = -std::stof(orientation_y)*DEG2RAD;
+
+        btScalar rotationAngleX = pitch; // Set this to the desired rotation angle around the x axis, in radians
+        btVector3 rotationAxisX(1, 0, 0); // x axis
+        btQuaternion rotationX(rotationAxisX, rotationAngleX);
+
+        btScalar rotationAngleZ = roll; // Set this to the desired rotation angle around the z axis, in radians
+        btVector3 rotationAxisZ(0, 0, 1); // z axis
+        btQuaternion rotationZ(rotationAxisZ, rotationAngleZ);
+
+        btQuaternion rotation = rotationX * rotationZ;
+
+        //btTransform tileTransform;
+        tileRigidBody->getMotionState()->getWorldTransform(tileTransform);
+        tileTransform.setRotation(rotation);
+        tileRigidBody->setWorldTransform(tileTransform);
+
         // Update
-        dynamicsWorld->stepSimulation(1 / 60.f, 10);
+        dynamicsWorld->stepSimulation(1 / 60.f, 100);
 
         // Update camera target
         //tileRigidBody->getMotionState()->getWorldTransform(tileTransform);
@@ -88,7 +151,10 @@ int main(int argc, char** argv) {
         btVector3 sphereOrigin = sphereTransform.getOrigin();
         sphereOrigin = sphereTransform.getOrigin();
         camera.target = (Vector3){sphereOrigin.getX(), sphereOrigin.getY(), sphereOrigin.getZ()};
-        camera.position = (Vector3){sphereOrigin.getX(), sphereOrigin.getY() + 10.0f, sphereOrigin.getZ() + 10.0f};;  // Camera position
+        camera.position = (Vector3){sphereOrigin.getX() + 5.0f, sphereOrigin.getY() + 20.0f, sphereOrigin.getZ() + 20.0f};;  // Camera position
+
+        // Tranformation matrix for rotations
+        cubeModel.transform = MatrixRotateXYZ((Vector3){ rotationAngleX, 0.0f, rotationAngleZ });
 
         // Draw
         BeginDrawing();
@@ -97,7 +163,8 @@ int main(int argc, char** argv) {
         BeginMode3D(camera);
 
         // Render cube
-        DrawCube((Vector3){tileOrigin.getX(), tileOrigin.getY(), tileOrigin.getZ()}, 20.0f, 2.0f, 20.0f, RED); // Adjust 
+        //DrawCube((Vector3){tileOrigin.getX(), tileOrigin.getY(), tileOrigin.getZ()}, 20.0f, 2.0f, 20.0f, RED); // Adjust 
+        DrawModel(cubeModel, (Vector3){tileOrigin.getX(), tileOrigin.getY(), tileOrigin.getZ()}, 1.0f, RED);   // Draw 3d model with texture
 
         // Draw sphere
         //btTransform sphereTransform;
@@ -109,6 +176,8 @@ int main(int argc, char** argv) {
 
         EndDrawing();
     }
+
+    UnloadModel(cubeModel);     // Unload model data
 
 
     // Cleanup
